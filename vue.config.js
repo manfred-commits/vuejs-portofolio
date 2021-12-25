20e16cef756749b7de94a70f52390138bed7a5c8
manfred-commits <manfredipiraino@gmail.com>
2021-12-25 23:41:28 +0100

    fix: ci-cd vue.config.js publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	publicPath: './',
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuejs-portofolio-basic/'
+    : '/',
 	chainWebpack: config => {
         config.module.rules.delete('eslint');
     }
