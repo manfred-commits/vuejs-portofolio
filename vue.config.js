018032ef5a69fac28ce65008f7428e1135a73241
manfred-commits <manfredipiraino@gmail.com>
2021-12-25 23:02:52 +0100

    fix: ci-cd

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	publicPath: './',
-    chainWebpack: config => {
-        config.module.rules.delete('eslint');
-    }
+	chainWebpack: config => {
+		config.module.rules.delete('eslint');
+    },
+	publicPath: './'
 }
